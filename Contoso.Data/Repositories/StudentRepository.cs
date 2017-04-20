57795075d09276f8675d7ddc88800b9eb4374c38
Abhilash Reddy <abhilash.reddy@outlook.com>
2017-04-20 11:31:44 -0400

    repositories

---
+++ Contoso.Data/Repositories/StudentRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contoso.Model;
+using InterviewQuestionBank.Data;
+
+namespace Contoso.Data.Repositories
+{
+    public class StudentRepository : GenericRepository<Student>, IStudentRepository
+    {
+        public StudentRepository(ContosoDbContext context) : base(context)
+        {
+        }
+    }
+
+    public interface IStudentRepository : IRepository<Student>
+    {
+    }
+}
